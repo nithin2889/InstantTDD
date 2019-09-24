10ca9ecb6cf0a4289cb2d5c8654aed26c75eb883
Nithin <nithin2889@gmail.com>
2019-09-24 23:39:38 +0530

    asserting an exception implementation

---
+++ src/test/java/com/instanttdd/junit/assertions/AssertExceptionsTest.java
@@ -0,0 +1,16 @@
+package com.instanttdd.junit.assertions;
+
+import java.io.IOException;
+import org.junit.Test;
+
+public class AssertExceptionsTest {
+
+  @Test(expected = IOException.class)
+  public void willThrowThrows() throws Exception {
+    willThrow();
+  }
+
+  private static void willThrow() throws IOException {
+    throw new IOException("Bang!!");
+  }
+}
