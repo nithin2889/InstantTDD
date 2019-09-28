ae48b3286b33ebcdd27599b405c70853f5f81cd2
Nithin <nithin2889@gmail.com>
2019-09-28 13:08:45 +0530

    expected exception rule

--- src/test/java/com/instanttdd/junit/assertions/AssertExceptionsRuleTest.java
+++ src/test/java/com/instanttdd/junit/assertions/AssertExceptionsRuleTest.java
@@ -1,5 +1,24 @@
 package com.instanttdd.junit.assertions;
 
-public class AssertExceptionsRuleTest {
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+public class AssertExceptionsRuleTest {
+  @Rule public ExpectedException expectedException = ExpectedException.none();
+
+  private static void willThrow() throws IOException {
+    throw new IOException("Bang");
+  }
+
+  @Test
+  public void willThrowError() throws Exception {
+    expectedException.expect(IOException.class);
+    expectedException.expectMessage("ang");
+    willThrow();
+  }
+
+  @Test
+  public void willThrowError2() throws Exception {}
 }
