74c4ac81134221ad0e8954fc8907875d6ff931b6
Nithin <nithin2889@gmail.com>
2019-09-28 13:02:56 +0530

    I/O test examples

--- src/test/java/com/instanttdd/readerwriter/ReaderWriterTest.java
+++ src/test/java/com/instanttdd/readerwriter/ReaderWriterTest.java
@@ -1,5 +1,33 @@
-import static org.junit.Assert.*;
+package com.instanttdd.readerwriter;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.List;
+import org.junit.Test;
 
 public class ReaderWriterTest {
+  @Test
+  public void readLinesItWrote() throws Exception {
+    // with a temporary folder to write to
+    File tempFolder = File.createTempFile("tmp", "file");
+    tempFolder.delete();
+    tempFolder.mkdir();
 
+    // when we write the file
+    List<String> someLines = asList("One", "Two", "Three");
+    File writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
+
+    // then we can read it back
+    List<String> readLines = ReaderWriter.read(writtenFile);
+
+    // and they match
+    assertEquals(someLines, readLines);
+
+    // and the tidy up is successful
+    assertTrue(writtenFile.delete());
+    assertTrue(tempFolder.delete());
+  }
 }
--- src/test/java/com/instanttdd/readerwriter/ReaderWriterWithBeforeAndAfterTest.java
+++ src/test/java/com/instanttdd/readerwriter/ReaderWriterWithBeforeAndAfterTest.java
@@ -1,33 +1,58 @@
 package com.instanttdd.readerwriter;
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class ReaderWriterTest {
-  @Test
-  public void readLinesItWrote() throws Exception {
+public class ReaderWriterWithBeforeAndAfterTest {
+
+  private File writtenFile;
+  private File tempFolder;
+
+  @Before
+  public void setUp() throws Exception {
     // with a temporary folder to write to
-    File tempFolder = File.createTempFile("tmp", "file");
+    tempFolder = File.createTempFile("tmp", "file");
     tempFolder.delete();
     tempFolder.mkdir();
+  }
 
+  @After
+  public void teardown() throws Exception {
+    // and the tidy up is successful
+    assertTrue(writtenFile.delete());
+    assertTrue(tempFolder.delete());
+  }
+
+  @Test
+  public void readLinesItWrote() throws Exception {
     // when we write the file
-    List<String> someLines = asList("One", "Two", "Three");
-    File writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
+    List<String> someLines = Arrays.asList("One", "Two", "Three");
+    writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
 
     // then we can read it back
     List<String> readLines = ReaderWriter.read(writtenFile);
 
     // and they match
     assertEquals(someLines, readLines);
+  }
 
-    // and the tidy up is successful
-    assertTrue(writtenFile.delete());
-    assertTrue(tempFolder.delete());
+  @Test
+  public void readZeroLinesItWrote() throws Exception {
+    // when we write the file
+    List<String> someLines = Arrays.asList();
+    writtenFile = ReaderWriter.write(tempFolder, "MyFile", someLines);
+
+    // then we can read it back
+    List<String> readLines = ReaderWriter.read(writtenFile);
+
+    // and they match
+    assertEquals(someLines, readLines);
   }
 }
