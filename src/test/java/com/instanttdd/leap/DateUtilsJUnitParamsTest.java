78a91b471efbeb40552d30059f30f53c0d00d4eb
Nithin <nithin2889@gmail.com>
2019-09-30 19:19:05 +0530

    JUnitParamsRunner test example

--- src/test/java/com/instanttdd/leap/DateUtilsJUnitParamsTest.java
+++ src/test/java/com/instanttdd/leap/DateUtilsJUnitParamsTest.java
@@ -1,5 +1,24 @@
 package com.instanttdd.leap;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitParamsRunner.class)
 public class DateUtilsJUnitParamsTest {
 
+  private Object parametersForIsLeapYear() {
+    return new Object[][] {{1992, true}, {1996, true}, {1991, false}, {1900, false}, {2000, true}};
+  }
+
+  @Test
+  @Parameters
+  @TestCaseName("The year {0} should have leap year as {1}")
+  public void isLeapYear(int year, boolean shouldBeLeap) {
+    assertThat(DateUtils.isLeapYear(year)).isEqualTo(shouldBeLeap);
+  }
 }
