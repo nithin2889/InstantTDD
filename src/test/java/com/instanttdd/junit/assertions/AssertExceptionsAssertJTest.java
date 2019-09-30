6de8b1be5cc65657f9112c1771ce458b4bea5140
Nithin <nithin2889@gmail.com>
2019-09-30 18:32:27 +0530

    AssertJ implementation examples

--- src/test/java/com/instanttdd/junit/assertions/AssertExceptionsAssertJTest.java
+++ src/test/java/com/instanttdd/junit/assertions/AssertExceptionsAssertJTest.java
@@ -1,5 +1,21 @@
 package com.instanttdd.junit.assertions;
 
-public class AssertExceptionsAssertJTest {
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.io.IOException;
+import org.junit.Test;
+
+public class AssertExceptionsAssertJTest {
+  private static void willThrow() throws IOException {
+    throw new IOException("Bang!!");
+  }
+
+  @Test
+  public void willThrowThrows() throws Exception {
+    assertThatThrownBy(AssertExceptionsAssertJTest::willThrow)
+        .isInstanceOf(IOException.class)
+        .hasMessage("Bang!!");
+
+    // We can do final checks here which is not possible in Hamcrest and JUnit
+  }
 }
